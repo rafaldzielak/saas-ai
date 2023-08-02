53ec31145a4524dedd7c52070d55718863a4372a
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-08-02 17:28:48 +0200

    [BE] Video generator API route

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import Heading from "@/components/Heading";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { VideoIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { VideoFormSchema, videoFormSchema } from "./constants";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Empty from "@/components/Empty";
+import Loader from "@/components/Loader";
+
+const VideoPage = () => {
+  const router = useRouter();
+  const [video, setVideo] = useState<string>();
+
+  const form = useForm<VideoFormSchema>({
+    resolver: zodResolver(videoFormSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: VideoFormSchema) => {
+    try {
+      setVideo(undefined);
+      const response = await axios.post("/api/video", values);
+      setVideo(response.data[0]);
+      form.reset();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title='Video generation'
+        description='Our most advanced video generation model.'
+        icon={VideoIcon}
+        iconColor='text-orange-700'
+        bgColor='bg-orange-700/10'
+      />
+      <div className='px-4 lg:px-8'>
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className='rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2'>
+              <FormField
+                name='prompt'
+                render={({ field }) => (
+                  <FormItem className='col-span-12 lg:col-span-10'>
+                    <FormControl className='m-0 p-0'>
+                      <Input
+                        className='border-0 outline-none focus-visible:ring-0 focus-visible::ring-transparent'
+                        disabled={isLoading}
+                        placeholder='Turn prompt into video!'
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button className='col-span-12 lg:col-span-2 w-full' disabled={isLoading}>
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className='space-y-4 mt-4'>
+          {isLoading && (
+            <div className='p-8 rounded-lg w-full flex items-center justify-center bg-muted'>
+              <Loader />
+            </div>
+          )}
+          {!video && !isLoading && (
+            <div>
+              <Empty label='No video generated' />
+            </div>
+          )}
+          {video && (
+            <video controls className='w-full aspect-video mt-8 rounded-lg border bg-black'>
+              <source src={video} />
+            </video>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPage;
